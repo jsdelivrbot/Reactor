6940ee1649af63341845ad532178c54424e0ce42
Steve Tickle <Steve@ZooKoo.com>
2017-01-12 06:43:35 +0000

    -

--- Outlet/Main.c
+++ Outlet/Main.c
@@ -8,6 +8,7 @@
 
 /*
 
+
 Hdr26   Funtion              ShiftReg       Hdr10
 ---------------------------------------------------------------
 1       3V3                 16/VCC          1/3V3
@@ -26,15 +27,14 @@ Hdr26   Funtion              ShiftReg       Hdr10
 
 
 Input bits->Port->Pin
-D0  - PA6   - 2
+D0  - PA15  - 2
 D1  - PA7   - 3
-D2  - PA15  - 4
+D2  - PA6   - 4
 D3  - PA0   - 5
 D4  - PA10  - 6
 D5  - PA2   - 7
 D6  - PA18  - 8
 D7  - PA19  - 9
-
 
 */
 
@@ -151,9 +151,9 @@ void SetLineState(bool d0, bool d1, bool d2, bool d3, bool d4, bool d5, bool d6,
 {
 	uint32_t 	portValue 	= portA->DAT;
 
-	SET_OR_CLEAR_BIT( portValue, 6,  d0 );
+	SET_OR_CLEAR_BIT( portValue, 15, d0 );
 	SET_OR_CLEAR_BIT( portValue, 7,  d1 );
-	SET_OR_CLEAR_BIT( portValue, 15, d2 );
+	SET_OR_CLEAR_BIT( portValue, 6,  d2 );
 	SET_OR_CLEAR_BIT( portValue, 0,  d3 );
 	SET_OR_CLEAR_BIT( portValue, 10, d4 );
 	SET_OR_CLEAR_BIT( portValue, 2,  d5 );
