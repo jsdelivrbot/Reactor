1abbf082f655efc08c08e1acfe273b500539de22
Steve Tickle <Steve@ZooKoo.com>
2017-01-25 22:11:58 +0000

    gerber generated

---
+++ Board/svgToDXF.sh
@@ -0,0 +1,33 @@
+# Convert SVG to EPS
+function svgToEps()
+{
+  if test $# -lt 1 ; then
+    echo "You need to pass in a filename." ; return
+  fi
+
+  epsfile=${1%.*}.eps
+
+  echo "inkscape -f $1 -E $epsfile"
+  inkscape -f $1 -E $epsfile &> /dev/null
+}
+
+function svgToDxf()
+{
+  if test $# -lt 1 ; then
+    echo "You need to pass in a filename." ; return
+  fi
+
+  base=${1%.*}
+  epsfile=${base}.eps
+  dxffile=${base}.dxf
+
+  svgToEps $1
+  echo "pstoedit -dt -f 'dxf:-polyaslines -mm' ${epsfile} ${dxffile}"
+  pstoedit -dt -f 'dxf:-polyaslines -mm' ${epsfile} ${dxffile} &> /dev/null
+  rm $epsfile
+}
+
+
+svgToDxf()
+
+
