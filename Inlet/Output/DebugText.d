b5b53a759aa7bb03d55a4e1ae5e9733e5131ad85
Steve Tickle <Steve@ZooKoo.com>
2017-01-28 20:49:02 +0000

    _

---
+++ Inlet/Output/DebugText.d
@@ -0,0 +1,8 @@
+DebugText.o: \
+ /home/steve/BlockWorks/ProjectLayout/SourceLibrary/Implementations/StdioDebugText/DebugText.c \
+ /home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/DebugText/DebugText.h \
+ /home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/Common/Common.h
+
+/home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/DebugText/DebugText.h:
+
+/home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/Common/Common.h:
---
+++ Inlet/Output/Main.d
@@ -0,0 +1,13 @@
+Main.o: ../../Inlet/Main.c \
+ /home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/DebugText/DebugText.h \
+ /home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/Common/Common.h \
+ /home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/Timestamp/Timestamp.h \
+ /home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/SharedMemory/SharedMemory.h
+
+/home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/DebugText/DebugText.h:
+
+/home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/Common/Common.h:
+
+/home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/Timestamp/Timestamp.h:
+
+/home/steve/BlockWorks/ProjectLayout/SourceLibrary/Interfaces/SharedMemory/SharedMemory.h:
