f1d5791f53abd3b5693cc6023a9972122ca91959
Steve Tickle <Steve@ZooKoo.com>
2017-03-30 23:20:17 +0100

    working ok again.

--- Inlet/Main.cpp
+++ Inlet/Main.cpp
@@ -197,6 +197,61 @@ volatile GPIOPort* 	gpio 	= 0;
 volatile GPIOPort*	portA	= 0;
 
 
+uint8_t*    counter64Base   = 0;
+volatile uint8_t*   counterControl;
+volatile uint32_t*   counterValue;
+
+
+
+void SetupCounter64()
+{
+    const uint32_t	    BASE 					= 0x01F01C00;	// CPUCFG
+    const uint32_t  	PAGE_SIZE 				= 4096;
+    const uint32_t		BASEPage 				= BASE & ~(PAGE_SIZE-1);
+    uint32_t 			BASEOffsetIntoPage		= BASE - BASEPage;
+    int					mem_fd					= 0;
+    uint8_t*			regAddrMap 				= NULL;
+
+
+    if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
+    {
+        perror("can't open /dev/mem");
+        exit (1);
+    }
+
+      regAddrMap = (uint8_t*)mmap(
+                        NULL,          
+                        BASEOffsetIntoPage+(PAGE_SIZE*2),       	
+                        PROT_READ|PROT_WRITE|PROT_EXEC,// Enable reading & writting to mapped memory
+                        MAP_SHARED,       //Shared with other processes
+                        mem_fd,           
+                        BASEPage);
+
+        if (regAddrMap == NULL) 
+        {
+              perror("mmap error");
+              close(mem_fd);
+              exit (1);
+        }
+
+    uint32_t*   pvalue = (uint32_t*)(regAddrMap + BASEOffsetIntoPage);
+
+    counter64Base   = (uint8_t*)pvalue;
+    //counterValue    = (volatile uint64_t*)(counter64Base+0x0284);
+    counterValue    = (volatile uint32_t*)(counter64Base+0x0284);
+
+    counterControl  = (uint8_t*)(counter64Base+0x0280);
+
+    //uint32_t*   counterControl  = (uint32_t*)(counter64Base+0x0280);
+}
+
+uint64_t GetCounter64()
+{
+    *counterControl     = 2;
+    uint32_t    value   = *counterValue;
+    return (uint64_t)value;
+}
+
 
 
 
@@ -232,8 +287,6 @@ int main()
     gpio 	= (GPIOPort*)SetupGPIO();
     portA	= &gpio[0];
 
-
-
     portA->CFG0 	= 0x11311111;
     portA->CFG1 	= 0x22211111;
     portA->CFG2 	= 0x11111111;
@@ -246,15 +299,21 @@ int main()
 
     uint16_t*   portA_DAT16   = (uint16_t*)&portA->DAT;
 
+    //
+    //
+    //
+    SetupCounter64();
+
     while(true)
     {
-
+#if 1
         //
         // Wait until a 1uSec boundary.
         //
-        const uint32_t  resolution  = 100;
         uint32_t endTimestamp = 0;
         __asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(endTimestamp) );
+
+        const uint32_t  resolution  = 20;
         endTimestamp    /= resolution;
         endTimestamp++;
         endTimestamp    *= resolution;
@@ -265,6 +324,30 @@ int main()
             __asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(timestamp) );
         } while(timestamp < endTimestamp);
 
+#else
+        uint32_t endTimestamp = 0;
+        *counterControl     = 2;
+        uint32_t    value   = *counterValue;
+
+        const uint32_t  resolution  = 24;
+        endTimestamp    /= resolution;
+        endTimestamp++;
+        endTimestamp    *= resolution;
+
+#if 0
+        uint32_t timestamp = 0;
+        do
+        {
+            *counterControl     = 2;
+            timestamp   = *counterValue;
+            for(uint32_t i=0; i<10; i++)
+            {
+                __asm volatile("nop" );
+            }
+        } while(timestamp < endTimestamp);
+#endif
+
+#endif
 
         uint16_t   inputValue  = *portA_DAT16;
         sharedMemory->inletToControl.Put( (uint8_t)inputValue );
